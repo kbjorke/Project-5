289a6ae87a8e9da182652eac9fd7774f53d8eb62
Kristian Bjørke <kbjorke@gmail.com>
2013-11-15 14:10:41 +0100

    Function class to be used when implementing integrand and weight functions for integral.

---
+++ Function.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+class Function
+{
+public:
+    Function();
+
+    virtual double operator()(double x){}
+    virtual double operator()(double *x){}
+
+    virtual ~Function(){}
+};
+
+#endif // FUNCTION_H
