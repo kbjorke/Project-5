2bf11b7d20c362eb07d3be8617e9a750c2a765fd
Kristian Bjørke <kbjorke@gmail.com>
2013-11-17 17:17:47 +0100

    Header and source for Integrate class, also including GaussLegendre subclass.

---
+++ Integral.cpp
@@ -0,0 +1,56 @@
+#include "lib.h"
+#include "Integral.h"
+
+Integral::Integral()
+{
+}
+
+
+GaussLegendre::GaussLegendre(double dimension)
+{
+    this->dimension = dimension;
+};
+
+void GaussLegendre::dimension_loops(int N, double *param, int ind, int *indices)
+{
+    int i;
+
+    if( ind == dimension )
+    {
+        term = dimension*func(*param);
+        for(i = 0; i < ind; i++ )
+        {
+            term *= w[indices[i]];
+        }
+        integral += term;
+    }
+    else
+    {
+        for( i = 0; i < N; i++ )
+        {
+            param[ind] = x[i];
+            indices[ind] = i;
+            dimension_loops(N, param, ind+1, indices);
+        }
+    }
+}
+
+double GaussLegendre::operator()(double lower, double upper,
+                  int n_points, Function &f)
+{
+    param = new double[dimension];
+    x = new double[n_points];
+    w = new double[n_points];
+
+    int *indices = new int[dimension];
+
+    func = f;
+
+    gauleg(lower, upper, x, w, n_points);
+
+    integral = 0;
+
+    dimension_loops(n_points, param, 0, indices);
+
+    return integral;
+};
---
+++ Integral.h
@@ -0,0 +1,40 @@
+#ifndef INTEGRAL_H
+#define INTEGRAL_H
+
+#include "Function.h"
+
+class Integral
+{
+protected:
+    double *params;
+    int dimension;
+
+    Function func;
+
+public:
+    Integral();
+
+    virtual double operator()(double lower, double upper,
+                              int n_points, Function &f){}
+
+    virtual ~Integral(){}
+};
+
+
+
+class GaussLegendre: public Integral
+{
+    private:
+
+        double integral, term;
+        double *param, *x, *w;
+
+        void dimension_loops(int N, double *param, int ind, int *indices);
+
+    public:
+
+        GaussLegendre(double dimension);
+        double operator()(double lower, double upper,
+                          int n_points, Function &f);
+};
+#endif // INTEGRAL_H
