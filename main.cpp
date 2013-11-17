6d44357dd03e812575c0cd2e14fd37d385efd9c3
Kristian Bjørke <kbjorke@gmail.com>
2013-11-17 14:17:41 +0100

    Made a functionality for dynamical nested loops, to use for integral loop over given dimensionality.

--- main.cpp
+++ main.cpp
@@ -19,18 +19,17 @@ class Integrand: public Function
     }
 
 
-    double operator()(double **r)
+    double operator()(double *r)
     {
-        mu = r[0][0]*r[0][0] + \
-                r[0][1]*r[0][1] + \
-                r[0][2]*r[0][2] + \
-                r[1][0]*r[1][0] + \
-                r[1][1]*r[1][1] + \
-                r[1][2]*r[1][2];
+        int i;
 
-        dist[0] = r[1][0] - r[0][0];
-        dist[1] = r[1][1] - r[0][1];
-        dist[2] = r[1][2] - r[0][2];
+        mu = 0;
+        for( i = 0; i < 3; i++ )
+        {
+            mu += r[0+i]*r[0+i] + r[3+i]*r[3+i];
+
+            dist[i] = r[3+i] - r[i];
+        }
 
         rho = sqrt(dist[0]*dist[0] + dist[1]*dist[1] + dist[2]*dist[2]);
 
@@ -38,8 +37,38 @@ class Integrand: public Function
     }
 };
 
+void inner_loops(int maxloop, int maxvalue, int ind, int *val)
+{
+    int i;
+
+    if( ind == maxloop )
+    {
+        for(i = 0; i < ind; i++ )
+        {
+            cout << val[i];
+        }
+        cout << endl;
+    }
+    else
+    {
+        for( i = 0; i < maxvalue; i++ )
+        {
+            val[ind] = i;
+            inner_loops(maxloop, maxvalue, ind+1, val);
+        }
+    }
+}
+
+void multi_loops(int maxloop, int maxvalue)
+{
+    int i;
+    int *val = new int[maxloop];
+
+    inner_loops(maxloop, maxvalue, 0, val);
+}
 
 
 int main(int argc, char *argv[])
 {
+    multi_loops(5,2);
 }
