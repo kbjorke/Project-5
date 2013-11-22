8a534d431bcd5bdeca809320caeafd34640ebeb2
Kristian Bjørke <kbjorke@gmail.com>
2013-11-22 12:15:11 +0100

    Made functionality to the Hamiltonian class.

--- Hamiltonian.cpp
+++ Hamiltonian.cpp
@@ -3,3 +3,17 @@
 Hamiltonian::Hamiltonian()
 {
 }
+
+double Hamiltonian::operator()(Function *psi, double *args)
+{
+    static double K;
+
+    K = - 0.5*psi->derivative2(args);
+
+    return K + (*V)(args);
+}
+
+void Hamiltonian::set_potential(Function *V)
+{
+    this->V = V;
+}
--- Hamiltonian.h
+++ Hamiltonian.h
@@ -1,10 +1,20 @@
 #ifndef HAMILTONIAN_H
 #define HAMILTONIAN_H
 
+#include "Function.h"
+
 class Hamiltonian
 {
+protected:
+    Function *V;
+
 public:
     Hamiltonian();
+
+    double operator()(Function *psi, double *args);
+    void set_potential(Function *V);
+
+    virtual ~Hamiltonian(){}
 };
 
 #endif // HAMILTONIAN_H
