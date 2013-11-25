e351ef9d911ba2abec1bf26e0ab6838e6a1c8cb4
Kristian Bjørke <kbjorke@gmail.com>
2013-11-25 13:59:00 +0100

    Added definitions of functions to be used for the VMC problem.

--- problem_definitions.cpp
+++ problem_definitions.cpp
@@ -3,9 +3,7 @@
 #include "problem_definitions.h"
 
 
-Integrand::Integrand(int dimension) : Function(dimension)
-{
-}
+Integrand::Integrand(int dimension) : Function(dimension){}
 
 double Integrand::set_params(double *params)
 {
@@ -36,3 +34,90 @@ double Integrand::operator()(double *r)
 }
 
 
+Psi_T1::Psi_T1(int dimension) : Function(dimension){}
+
+double Psi_T1::set_params(double *params)
+{
+    alpha = *params;
+}
+
+double Psi_T1::operator()(double *r)
+{
+    int i;
+
+    mu = 0;
+    for( i = 0; i < 3; i++ )
+    {
+        mu += r[0+i]*r[0+i] + r[3+i]*r[3+i];
+    }
+
+    return exp(-alpha*alpha*mu*0.5);
+}
+
+
+Psi_T2::Psi_T2(int dimension) : Function(dimension){}
+
+double Psi_T2::set_params(double *params)
+{
+    alpha = params[0];
+    beta = params[1];
+}
+
+double Psi_T2::operator()(double *r)
+{
+    int i;
+
+    mu = 0;
+    for( i = 0; i < 3; i++ )
+    {
+        mu += r[0+i]*r[0+i] + r[3+i]*r[3+i];
+
+        dist[i] = r[3+i] - r[i];
+    }
+    rho = sqrt(dist[0]*dist[0] + dist[1]*dist[1] + dist[2]*dist[2]);
+
+    exponent = -alpha*alpha*mu*0.5 + rho*0.5/(1 + beta*rho);
+
+    return exp(exponent);
+}
+
+V_TOT::V_TOT(int dimension) : Function(dimension){}
+
+double V_TOT::operator()(double *r)
+{
+    int i;
+
+    mu = 0;
+    for( i = 0; i < 3; i++ )
+    {
+        mu += r[0+i]*r[0+i] + r[3+i]*r[3+i];
+
+        dist[i] = r[3+i] - r[i];
+    }
+    rho = sqrt(dist[0]*dist[0] + dist[1]*dist[1] + dist[2]*dist[2]);
+
+    if( rho == 0 )
+    {
+        return 0;
+    }
+    else
+    {
+        return 0.5*mu + 1/rho;
+    }
+}
+
+
+V_HO::V_HO(int dimension) : Function(dimension){}
+
+double V_HO::operator()(double *r)
+{
+    int i;
+
+    mu = 0;
+    for( i = 0; i < 3; i++ )
+    {
+        mu += r[0+i]*r[0+i] + r[3+i]*r[3+i];
+    }
+
+    return 0.5*mu;
+}
--- problem_definitions.h
+++ problem_definitions.h
@@ -15,4 +15,48 @@ class Integrand: public Function
     double operator()(double *r);
 };
 
+class Psi_T1: public Function
+{
+    private:
+    double alpha, mu;
+
+    public:
+    Psi_T1(int dimension);
+    double set_params(double *params);
+    double operator()(double *r);
+};
+
+class Psi_T2: public Function
+{
+    private:
+    double alpha, beta, mu, rho, exponent;
+    double dist[3];
+
+    public:
+    Psi_T2(int dimension);
+    double set_params(double *params);
+    double operator()(double *r);
+};
+
+class V_TOT: public Function
+{
+    private:
+    double mu, rho;
+    double dist[3];
+
+    public:
+    V_TOT(int dimension);
+    double operator()(double *r);
+};
+
+class V_HO: public Function
+{
+    private:
+    double mu;
+
+    public:
+    V_HO(int dimension);
+    double operator()(double *r);
+};
+
 #endif // PROBLEM_DEFINITIONS_H
