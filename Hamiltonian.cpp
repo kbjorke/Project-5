fa9ce1a19276de39f2a6f8eb038b0ef38f5d05d6
Kristian Bjørke <kbjorke@gmail.com>
2013-11-26 20:58:49 +0100

    Made proper comments to the Hamiltonian class header and source.

--- Hamiltonian.cpp
+++ Hamiltonian.cpp
@@ -1,22 +1,42 @@
 #include "Hamiltonian.h"
 
-#include <iostream>
-
 using namespace std;
 
-Hamiltonian::Hamiltonian()
-{
-}
+Hamiltonian::Hamiltonian(){}
 
+/* Method operator().
+ *
+ * Used to apply the Hamiltonian to a state or wavefunction
+ * at a given position/arguments.
+ *
+ * Input:
+ * 			*psi  : Pointer to a function object containing the
+ * 					wavefunction to apply the Hamiltonian to.
+ *
+ *			*args : Pointer or array containing the parameters
+ * 					where the Hamiltonian and wavefunction will
+ * 					interact.
+ * */
 double Hamiltonian::operator()(Function *psi, double *args)
 {
     static double K;
 
-    K = - 0.5*psi->derivative2(args);
+    K = - 0.5*(*psi).derivative2(args);
 
     return K + (*V)(args);
 }
 
+/* Method set_potential.
+ *
+ * Used to set the potential for the Hamiltonian.
+ *
+ * Input:
+ * 			*V : Pointer to a function object containing the
+ *				 potential to be added.
+ *
+ * NOTE: Mights add implementation or seperate method to add a
+ * 		 new/extra potentials to the Hamiltonian.
+ * */
 void Hamiltonian::set_potential(Function *V)
 {
     this->V = V;
--- Hamiltonian.h
+++ Hamiltonian.h
@@ -3,6 +3,18 @@
 
 #include "Function.h"
 
+/* Class for implementation of Hamiltonian.
+ *
+ * Used as a superclass to create a sub-class which implement a
+ * Hamiltonian to be used for Quantum Mechanical problems.
+ *
+ * Methods:
+ * 			- contructor     : Used to create Hamiltonian object.
+ * 			- operator()     : Used to apply the Hamiltonian to a state or
+ *							   wavefunction at a specific point.
+ * 			- set_potential  : Used to set a external potential to the
+ *							   Hamiltonian.
+ * */
 class Hamiltonian
 {
 protected:
